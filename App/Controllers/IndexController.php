e14edac0fd61b0bcc7933d99ad4ee330a3d932fe
lemelinha <lucas.leme.0806@gmail.com>
2024-06-07 12:45:48 -0300

    index page

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+use Needs\Controller\Controller;
+
+class IndexController extends Controller {
+    public function index() {
+        $this->pageTitle = 'ProjetoX';
+        $this->renderView('index');
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+    ini_set('session.gc_maxlifetime', 3600*24*7*2);
+    ini_set("session.cookie_lifetime", 0);
+
     session_start();
     require '../vendor/autoload.php';
 
